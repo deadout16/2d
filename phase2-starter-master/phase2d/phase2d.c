caaef497edbd4e32422e8c08fb9a7f1aa392d1a1
hackerrank <me@hackerrank.com>
2021-04-05 21:33:56 -0700

    added sys handlers

--- phase2-starter-master/phase2d/phase2d.c
+++ phase2-starter-master/phase2d/phase2d.c
@@ -9,6 +9,21 @@
 
 #include "phase2Int.h"
 
+// keeps track of locks created by user
+typedef struct locks{
+    int lockId;
+    locks *next;
+} locks;
+
+// keeps track of condition variable created by user
+typedef struct conds{
+    int condId;
+    conds *next;
+} conds;
+
+locks *lockHead;
+conds *condHead;
+
 int P2_Startup(void *arg)
 {
     int rc, pid;
@@ -16,7 +31,50 @@ int P2_Startup(void *arg)
     P2ClockInit();
     P2DiskInit();
 
+    // malloc and set the head node values for lists
+    lockHead = (locks *)malloc(sizeof(locks));
+    lockHead.lockId = -1;
+    lockHead.next = NULL;
+    condHead = (conds *)malloc(sizeof(conds));
+    condHead.condId = -1;
+    condHead.next = NULL;
+
     // install system call handlers
+    rc = P2_SetSyscallHandler(SYS_LOCKCREATE, lockCreateStub);
+    assert(rc == P1_SUCCESS);
+
+    rc = P2_SetSyscallHandler(SYS_LOCKFREE, lockFreeStub);
+    assert(rc == P1_SUCCESS);
+
+    rc = P2_SetSyscallHandler(SYS_LOCKNAME, lockNameStub);
+    assert(rc == P1_SUCCESS);
+
+    rc = P2_SetSyscallHandler(SYS_LOCKNAME, lockNameStub);
+    assert(rc == P1_SUCCESS);
+
+    rc = P2_SetSyscallHandler(SYS_LOCKACQUIRE, lockAcquireStub);
+    assert(rc == P1_SUCCESS);
+
+    rc = P2_SetSyscallHandler(SYS_LOCKRELEASE, lockReleaseStub);
+    assert(rc == P1_SUCCESS);
+
+    rc = P2_SetSyscallHandler(SYS_CONDCREATE, condCreateStub);
+    assert(rc == P1_SUCCESS);
+
+    rc = P2_SetSyscallHandler(SYS_CONDFREE, condFreeStub);
+    assert(rc == P1_SUCCESS);
+
+    rc = P2_SetSyscallHandler(SYS_CONDNAME, condNameStub);
+    assert(rc == P1_SUCCESS);
+
+    rc = P2_SetSyscallHandler(SYS_CONDWAIT, condWaitStub);
+    assert(rc == P1_SUCCESS);
+
+    rc = P2_SetSyscallHandler(SYS_CONDSIGNAL, condSignalStub);
+    assert(rc == P1_SUCCESS);
+
+    rc = P2_SetSyscallHandler(SYS_CONDBROADCAST, condBroadcastStub);
+    assert(rc == P1_SUCCESS);
 
     rc = P2_Spawn("P3_Startup", P3_Startup, NULL, 4*USLOSS_MIN_STACK, 2, &pid);
     assert(rc == P1_SUCCESS);
@@ -29,3 +87,4 @@ int P2_Startup(void *arg)
     return 0;
 }
 
+int P2_lockCreate()
